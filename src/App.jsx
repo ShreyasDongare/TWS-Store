4a27870975e936388e4ab525e48c2580d747b404
Shreyas <dongareshreyas@gmail.com>
2022-12-30 14:34:54 +0530

    worked on data, images, and product components, added router dom, mapped over product data also worked on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import {BrowserRouter , Route, Routes } from "react-router-dom";
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Products from './pages/Products';
 
 const App = () => {
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+      
+    </BrowserRouter>
+  );
 }
 
 export default App
